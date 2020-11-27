895e2c8b376e2a098363edd838031d424b100802
Akshar <mistry.aks@gmail.com>
2020-11-27 21:59:33 +0530

    cleanup web client

--- web-client/src/App.tsx
+++ web-client/src/App.tsx
@@ -1,25 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Provider, defaultTheme} from '@adobe/react-spectrum';
+import Layout from './components/layout';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Provider UNSAFE_className="App" theme={defaultTheme} colorScheme="light">
+        <Layout/>
+      </Provider>
   );
 }
 
