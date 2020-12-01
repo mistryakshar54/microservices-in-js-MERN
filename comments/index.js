4f2a780252105ae3257d80eeb9eac8a8f4e887a1
Akshar <mistry.aks@gmail.com>
2020-12-01 16:08:25 +0530

    scaffold event observers

--- comments/index.js
+++ comments/index.js
@@ -2,6 +2,7 @@ const express  = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors')
+const axios = require('axios');
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -15,17 +16,25 @@ app.get('/api/posts/:id/comments' , (req , res) => {
     res.status(200).json({ message : 'success' , data : comments , length : comments.length })
 });
 
-app.post('/api/posts/:id/comments' , jsonParser ,(req , res) => {
+app.post('/api/posts/:id/comments' , jsonParser , async(req , res) => {
     const { comment } = req.body;
     const id = new Date().getTime();
     const comments = commentsByPostId[req.params.id] || [];
     comments.push( { comment , id } );
     commentsByPostId[req.params.id] = comments;
+
+    await axios.post('http://localhost:8005/api/event' , {type : 'CommentCreated' , message : { comment , id }});
+  
     res.status(200).json({ message : 'success' , data : comments , length : comments.length })
 });
 
+app.post('/api/event' , jsonParser ,async(req , res) => {
+    const { type , message } = req.body;
+    console.log("New event recieved" , type , message);
+    res.status(200).json({ message : 'success' , data : {type , message} })
+});
 
 const server = app.listen( 8001 );
 server.on('listening' , () => {
-    console.log("Server listening on 8001");
+    console.log("Comments Service listening on 8001");
 })
--- posts/index.js
+++ posts/index.js
@@ -2,6 +2,7 @@ const express  = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors')
+const axios = require('axios');
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -12,15 +13,21 @@ const posts = [];
 
 app.get('/api/posts' , (req , res) => res.status(200).json({ message : 'success' , data : posts , length : posts.length }));
 
-app.post('/api/posts' , jsonParser ,(req , res) => {
+app.post('/api/posts' , jsonParser ,async(req , res) => {
     const { title } = req.body;
     const id = new Date().getTime();
     posts.push( { title , id } );
+    await axios.post('http://localhost:8005/api/event' , {type : 'PostCreated' , message : { title , id }});
     res.status(200).json({ message : 'success' , data : { title , id } , length : 1 })
 });
 
+app.post('/api/event' , jsonParser ,async(req , res) => {
+    const { type , message } = req.body;
+    console.log("New event recieved" , type , message);
+    res.status(200).json({ message : 'success' , data : {type , message} })
+});
 
 const server = app.listen( 8000 );
 server.on('listening' , () => {
-    console.log("Server listening on 8000");
+    console.log("Post Service listening on 8000");
 })
