c5fcc99d52284af6f636306179e8a391f332c49b
Akshar <mistry.aks@gmail.com>
2020-12-04 21:53:41 +0530

    Log events in event bus

--- event-bus/index.js
+++ event-bus/index.js
@@ -9,16 +9,21 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 const jsonParser = bodyParser.json()
 
+const eventLogs = [];
+
 app.post('/api/event' , jsonParser ,async(req , res) => {
     const { type , message } = req.body;
     console.log("New event recieved" , type , message);
-    await axios.post( 'http://localhost:8000/api/event' , { type , message } );    
-    await axios.post( 'http://localhost:8001/api/event' , { type , message } );    
-    await axios.post( 'http://localhost:8002/api/event' , { type , message } );    
-
+     axios.post( 'http://localhost:8000/api/event' , { type , message } );    
+     axios.post( 'http://localhost:8001/api/event' , { type , message } );    
+     axios.post( 'http://localhost:8002/api/event' , { type , message } );    
+    eventLogs.push({ type , message });
     res.status(200).json({ message : 'success' , data : "Event Sent" })
 });
 
+app.get('/api/event' , async( req,res ) => {
+    res.status(200).json({ message : 'success' , eventLogs })
+});
 
 const server = app.listen( 8005 );
 server.on('listening' , () => {
