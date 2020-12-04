195fa6c49eada3bf250b073029ceb781b9a5046b
Akshar <mistry.aks@gmail.com>
2020-12-04 21:54:07 +0530

    Enable fetching events from event bus

--- query-service/index.js
+++ query-service/index.js
@@ -10,8 +10,8 @@ app.use(bodyParser.urlencoded({ extended: false }))
 const jsonParser = bodyParser.json()
 
 const posts = {};
-app.post('/api/event' , jsonParser ,async(req , res) => {
-    const { type , message } = req.body;
+
+const handleEvents = (type , message) => {
     switch( type ){
         case 'PostCreated' : {
             posts[message.id] = message;
@@ -24,6 +24,11 @@ app.post('/api/event' , jsonParser ,async(req , res) => {
             break;
         }
     }
+}
+
+app.post('/api/event' , jsonParser ,async(req , res) => {
+    const { type , message } = req.body;
+    handleEvents( type, message );
     console.log("New event recieved" , type , message);
     res.status(200).json({ message : 'success' , data : {type , message} })
 });
@@ -31,6 +36,12 @@ app.post('/api/event' , jsonParser ,async(req , res) => {
 app.get('/api/posts' , (req , res) => res.status(200).json({ message : 'success' , data : posts , length : posts.length }));
 
 const server = app.listen( 8002 );
-server.on('listening' , () => {
+server.on('listening' , async() => {
     console.log("Query Service listening on 8002");
+    const {data} = await axios.get('http://localhost:8005/api/event');
+    const { eventLogs } = data;
+    console.log("eventLogs", eventLogs)
+    eventLogs.forEach( log => {
+        handleEvents( log.type, log.message );
+    });
 })
