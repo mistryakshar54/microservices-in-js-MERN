dcd52104778fcf4a6fbb6990417b7096268c49ec
Akshar <mistry.aks@gmail.com>
2020-12-17 12:50:33 +0530

    Wire up custom error middleware

--- ticketing-app/auth/src/index.ts
+++ ticketing-app/auth/src/index.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 import { json } from 'body-parser';
 import {authRoutes} from './routes/index';
+import { errorHandler } from './middleware/errorHandler';
+
 const app = express();
 app.use(json());
 
 app.get('/api/users/currentUser' ,( req , res ) => res.status(200).json({message : "This is the current user"}));
 app.use('/api/users/auth' ,authRoutes);
+
+app.use(errorHandler);
 
 app.listen( 3000 , ()=> console.log('App listening on 3000!!!!'));
