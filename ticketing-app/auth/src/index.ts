49395429ef033c0a745497c5fed3b9d08eedac7e
Akshar <mistry.aks@gmail.com>
2020-12-17 10:23:13 +0530

    Add signup route and bootstrap json validator

--- ticketing-app/auth/src/index.ts
+++ ticketing-app/auth/src/index.ts
@@ -5,7 +5,6 @@ const app = express();
 app.use(json());
 
 app.get('/api/users/currentUser' ,( req , res ) => res.status(200).json({message : "This is the current user"}));
-app.get('/api/users/auth/abc' ,( req , res ) => res.status(200).json({message : "This is the ABc route"}));
 app.use('/api/users/auth' ,authRoutes);
 
 app.listen( 3000 , ()=> console.log('App listening on 3000!!!!'));
--- ticketing-app/auth/src/routes/index.ts
+++ ticketing-app/auth/src/routes/index.ts
@@ -1,7 +1,20 @@
-import { Router } from 'express';
+import { Router, Request , Response } from 'express';
+import { body, validationResult } from 'express-validator';
 
 const router = Router();
-router.get('/signup' , (req, res) => {res.status(200).json("Signup")});
+router.post('/signup' , [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password').trim().isLength({ min : 4 , max: 20 }).withMessage("Password must be min 4 and max 20 chars")
+],(req : Request , res : Response) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        res.status(400).json({message : errors.array()});
+    }
+    const {email , password} = req.body;
+    console.log('User created!!');
+    res.status(200).json({message : errors.array()});
+
+});
 router.post('/signin' , (req, res) => {});
 router.post('/signout' , (req, res) => {});
 
