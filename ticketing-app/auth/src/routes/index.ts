ea0d5cc36cbc0766e963e6e324e1ae0c5eb787a6
Akshar <mistry.aks@gmail.com>
2020-12-23 15:57:33 +0530

    Add bad request error

--- ticketing-app/auth/src/routes/index.ts
+++ ticketing-app/auth/src/routes/index.ts
@@ -1,22 +1,26 @@
 import { Router, Request , Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import { BadRequestError } from '../errors/badRequestError';
 import { RequestValidationError } from '../errors/requestValidationError';
-import { DBConnectionError } from '../errors/dbConError';
+import { User } from '../models/users';
 
 const router = Router();
 router.post('/signup' , [
     body('email').isEmail().withMessage('Email must be valid'),
     body('password').trim().isLength({ min : 4 , max: 20 }).withMessage("Password must be min 4 and max 20 chars")
-],(req : Request , res : Response) => {
+],async (req : Request , res : Response) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()){
        throw new RequestValidationError(errors.array());
     }
     const {email , password} = req.body;
-    console.log('User created!!');
-    throw new DBConnectionError();
-    //res.status(200).json({message : errors.array()});
-
+    const existingUser = await User.findOne({ email });
+    if(existingUser){
+        throw new BadRequestError('Email already in use');
+    }
+    const user = User.buildUser({ email, password })
+    await user.save();
+    res.status(201).send({ message : 'success', data : user });
 });
 router.post('/signin' , (req, res) => {});
 router.post('/signout' , (req, res) => {});
