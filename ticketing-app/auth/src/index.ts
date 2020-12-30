9d5a2320203a6bbb5105515b24844a2e95c5ec72
Akshar <mistry.aks@gmail.com>
2020-12-30 10:30:20 +0530

    Wire up jwt and cookie in for auth

--- ticketing-app/auth/src/index.ts
+++ ticketing-app/auth/src/index.ts
@@ -1,12 +1,18 @@
 import express from 'express';
+import 'express-async-errors';
 import { json } from 'body-parser';
 import {authRoutes} from './routes/index';
 import { errorHandler } from './middleware/errorHandler';
 import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
+
 const MONGO_URL = "mongodb://auth-mongo-srv:27017/users";
 
 const app = express();
+app.set('trust proxy', true); //Make sure express is behind ingress-nginx and thus allows the flow
 app.use(json());
+app.use( cookieSession({ signed: false, httpOnly: true, }) );
+
 
 app.get('/api/users/currentUser' ,( req , res ) => res.status(200).json({message : "This is the current user"}));
 app.use('/api/users/auth' ,authRoutes);
@@ -14,6 +20,10 @@ app.use('/api/users/auth' ,authRoutes);
 app.use(errorHandler);
 
 const bootStrap = async() => {
+
+    if(!process.env.JWT_KEY){
+        throw new Error("ENV Variables not confifugred!!");   
+    }
     try{
         await mongoose.connect( MONGO_URL , {
             useNewUrlParser: true,
--- ticketing-app/auth/src/routes/index.ts
+++ ticketing-app/auth/src/routes/index.ts
@@ -3,12 +3,13 @@ import { body, validationResult } from 'express-validator';
 import { BadRequestError } from '../errors/badRequestError';
 import { RequestValidationError } from '../errors/requestValidationError';
 import { User } from '../models/users';
+import jwt from 'jsonwebtoken';
 
 const router = Router();
 router.post('/signup' , [
     body('email').isEmail().withMessage('Email must be valid'),
     body('password').trim().isLength({ min : 4 , max: 20 }).withMessage("Password must be min 4 and max 20 chars")
-],async (req : Request , res : Response) => {
+], async(req : Request , res : Response) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()){
        throw new RequestValidationError(errors.array());
@@ -18,8 +19,14 @@ router.post('/signup' , [
     if(existingUser){
         throw new BadRequestError('Email already in use');
     }
-    const user = User.buildUser({ email, password })
+    const user = User.buildUser({ email, password }) 
     await user.save();
+    //Generate JWT
+    const jwtToken = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_KEY!)
+    req.session = {
+        jwt : jwtToken
+    }
+    //Store the session
     res.status(201).send({ message : 'success', data : user });
 });
 router.post('/signin' , (req, res) => {});
