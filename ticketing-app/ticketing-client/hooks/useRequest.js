606366b784e4eefd9575d84f710e83026f654ef0
Akshar <mistry.aks@gmail.com>
2021-01-07 10:41:06 +0530

    Add custom hook for data reterival

---
+++ ticketing-app/ticketing-client/hooks/useRequest.js
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import axios from 'axios';
+
+const useRequest = ( endPoint , method , body = null ) => {
+    const [errors, setErrors] = useState([]);
+    const [isLoading, setLoading] = useState(false);
+    const doResponse = async() => {
+        setLoading(true);
+        try{
+            const response = await axios[method]( endPoint, body );
+            setErrors([]);
+            return response.data;
+        }
+        catch(err){
+            console.log(err.response.data);
+            setErrors(err.response.data);
+        }
+        setLoading(false);
+    }
+    return {doResponse , isLoading, errors };
+}
+export default useRequest;
