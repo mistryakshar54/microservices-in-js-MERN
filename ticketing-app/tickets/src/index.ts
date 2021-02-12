3748e9e98eda333a3c27bf5845673b205521c01e
Akshar <mistry.aks@gmail.com>
2021-02-12 18:49:23 +0530

    create ticketing service

---
+++ ticketing-app/tickets/src/index.ts
@@ -0,0 +1,52 @@
+import  jwt  from 'jsonwebtoken';
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import {authRoutes} from './routes/index';
+import { errorHandler } from '@amdevcorp/ticketing-common';
+import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
+
+const MONGO_URL = "mongodb://auth-mongo-srv:27017/users";
+
+const app = express();
+app.set('trust proxy', true); //Make sure express is behind ingress-nginx and thus allows the flow
+app.use(json());
+app.use( cookieSession({ signed: false, httpOnly: true, }) );
+
+
+app.get('/api/users/currentUser' ,( req , res ) => {
+    if( !req.session?.jwt ){
+        res.status(200).json({currentUser : null});
+    }
+    try{
+        const decodedToken = jwt.verify( req.session?.jwt , process.env.JWT_KEY!  )
+        res.status(200).json({currentUser : decodedToken});
+    }catch{
+        res.status(200).json({currentUser : null});
+    }  
+});
+app.use('/api/users/auth' ,authRoutes);
+
+app.use(errorHandler);
+
+const bootStrap = async() => {
+
+    if(!process.env.JWT_KEY){
+        throw new Error("ENV Variables not confifugred!!");   
+    }
+    try{
+        await mongoose.connect( MONGO_URL , {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useUnifiedTopology: true
+        });
+        console.log("Connected to db");
+        app.listen( 3000 , ()=> console.log('App listening on 3000!!!!'));
+    }
+    catch(err){
+        console.log("Error connecting to mongo", err);
+    }
+}
+
+bootStrap();
---
+++ ticketing-app/tickets/src/routes/index.ts
@@ -0,0 +1,55 @@
+import { Router, Request , Response } from 'express';
+import { body } from 'express-validator';
+import { BadRequestError, validationHandler } from '@amdevcorp/ticketing-common';
+import { User } from '../models/users';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+
+const router = Router();
+router.post('/signup' , [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password').trim().isLength({ min : 4 , max: 20 }).withMessage("Password must be min 4 and max 20 chars")
+], validationHandler, async(req : Request , res : Response) => {
+    const {email , password} = req.body;
+    const existingUser = await User.findOne({ email });
+    if(existingUser){
+        throw new BadRequestError('Email already in use');
+    }
+    const user = User.buildUser({ email, password }) 
+    await user.save();
+    //Generate JWT
+    const jwtToken = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_KEY!)
+    req.session = {
+        jwt : jwtToken
+    }
+    //Store the session
+    res.status(201).send({ message : 'success', data : user });
+});
+router.post('/signin',[ 
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password').trim().notEmpty().withMessage("Password must not be empty")
+], validationHandler , async(req : Request , res : Response) => {
+    const {email , password} = req.body;
+    const existingUser = await User.findOne({ email });
+    if(!existingUser){
+        throw new BadRequestError('Invalid Credentails');
+    }
+    const isValidPwd = bcrypt.compare( password, existingUser.password );
+    if( !isValidPwd ){
+        throw new BadRequestError('Invalid Credentails');
+    }
+    //Generate JWT
+    const jwtToken = jwt.sign({ id: existingUser.id, email: existingUser.email }, process.env.JWT_KEY!)
+    req.session = {
+        jwt : jwtToken
+    }
+    //Store the session
+    res.status(200).send({ message : 'success', data : existingUser });
+
+});
+router.post('/signout' , (req, res) => {
+    req.session = null;
+    res.status(200).send({ message : 'success', data : 'User signed out!' });
+});
+
+export {router as authRoutes};
