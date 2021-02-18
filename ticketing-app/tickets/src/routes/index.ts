cca91e2c69f66b5b0935cb746045cf866aee5766
Akshar <mistry.aks@gmail.com>
2021-02-18 13:47:41 +0530

    setup jest

--- ticketing-app/tickets/src/routes/index.ts
+++ ticketing-app/tickets/src/routes/index.ts
@@ -1,55 +1,58 @@
 import { Router, Request , Response } from 'express';
 import { body } from 'express-validator';
-import { BadRequestError, validationHandler } from '@amdevcorp/ticketing-common';
-import { User } from '../models/users';
+import { BadRequestError, validationHandler, requireAuth } from '@amdevcorp/ticketing-common';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 
 const router = Router();
-router.post('/signup' , [
-    body('email').isEmail().withMessage('Email must be valid'),
-    body('password').trim().isLength({ min : 4 , max: 20 }).withMessage("Password must be min 4 and max 20 chars")
-], validationHandler, async(req : Request , res : Response) => {
-    const {email , password} = req.body;
-    const existingUser = await User.findOne({ email });
-    if(existingUser){
-        throw new BadRequestError('Email already in use');
-    }
-    const user = User.buildUser({ email, password }) 
-    await user.save();
-    //Generate JWT
-    const jwtToken = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_KEY!)
-    req.session = {
-        jwt : jwtToken
-    }
-    //Store the session
-    res.status(201).send({ message : 'success', data : user });
+router.post( '/tickets', requireAuth , async(req : Request , res : Response) => {
+    console.log("Henlooo!!! Came here");
+    res.status(200).send({message : 'success'});
 });
-router.post('/signin',[ 
-    body('email').isEmail().withMessage('Email must be valid'),
-    body('password').trim().notEmpty().withMessage("Password must not be empty")
-], validationHandler , async(req : Request , res : Response) => {
-    const {email , password} = req.body;
-    const existingUser = await User.findOne({ email });
-    if(!existingUser){
-        throw new BadRequestError('Invalid Credentails');
-    }
-    const isValidPwd = bcrypt.compare( password, existingUser.password );
-    if( !isValidPwd ){
-        throw new BadRequestError('Invalid Credentails');
-    }
-    //Generate JWT
-    const jwtToken = jwt.sign({ id: existingUser.id, email: existingUser.email }, process.env.JWT_KEY!)
-    req.session = {
-        jwt : jwtToken
-    }
-    //Store the session
-    res.status(200).send({ message : 'success', data : existingUser });
+// router.post('/signup' , [
+//     body('email').isEmail().withMessage('Email must be valid'),
+//     body('password').trim().isLength({ min : 4 , max: 20 }).withMessage("Password must be min 4 and max 20 chars")
+// ], validationHandler, async(req : Request , res : Response) => {
+//     const {email , password} = req.body;
+//     const existingUser = await User.findOne({ email });
+//     if(existingUser){
+//         throw new BadRequestError('Email already in use');
+//     }
+//     const user = User.buildUser({ email, password }) 
+//     await user.save();
+//     //Generate JWT
+//     const jwtToken = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_KEY!)
+//     req.session = {
+//         jwt : jwtToken
+//     }
+//     //Store the session
+//     res.status(201).send({ message : 'success', data : user });
+// });
+// router.post('/signin',[ 
+//     body('email').isEmail().withMessage('Email must be valid'),
+//     body('password').trim().notEmpty().withMessage("Password must not be empty")
+// ], validationHandler , async(req : Request , res : Response) => {
+//     const {email , password} = req.body;
+//     const existingUser = await User.findOne({ email });
+//     if(!existingUser){
+//         throw new BadRequestError('Invalid Credentails');
+//     }
+//     const isValidPwd = bcrypt.compare( password, existingUser.password );
+//     if( !isValidPwd ){
+//         throw new BadRequestError('Invalid Credentails');
+//     }
+//     //Generate JWT
+//     const jwtToken = jwt.sign({ id: existingUser.id, email: existingUser.email }, process.env.JWT_KEY!)
+//     req.session = {
+//         jwt : jwtToken
+//     }
+//     //Store the session
+//     res.status(200).send({ message : 'success', data : existingUser });
 
-});
-router.post('/signout' , (req, res) => {
-    req.session = null;
-    res.status(200).send({ message : 'success', data : 'User signed out!' });
-});
+// });
+// router.post('/signout' , (req, res) => {
+//     req.session = null;
+//     res.status(200).send({ message : 'success', data : 'User signed out!' });
+// });
 
-export {router as authRoutes};
+export {router as ticketRoutes};
