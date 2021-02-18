d1a1c890650b3c07a17fa05e22d81bdd71c290e9
Akshar <mistry.aks@gmail.com>
2021-02-18 13:47:56 +0530

    Write route tests for TDD

--- ticketing-app/tickets/src/routes/__test__/new.test.ts
+++ ticketing-app/tickets/src/routes/__test__/new.test.ts
@@ -1,1 +1,9 @@
-// import request from 
+import request from 'supertest';
+import { app } from '../../app';
+
+it('returns an error if the user is not authenticated' , async() => {
+    const resp = await request(app)
+    .post('/api/tickets')
+    .send({});
+    expect(resp.status).toEqual(403);
+})
