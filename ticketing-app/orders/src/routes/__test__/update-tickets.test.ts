91af761fc0febbcfa807de794f34ada340d86c39
Akshar <mistry.aks@gmail.com>
2021-03-15 17:47:13 +0530

    Delete Orders

--- ticketing-app/orders/src/routes/__test__/update-tickets.test.ts
+++ ticketing-app/orders/src/routes/__test__/update-tickets.test.ts
@@ -1,6 +1,6 @@
 import request from 'supertest';
 import { app } from '../../app';
-import { Ticket } from '../../models/Orders';
+import { Ticket } from '../../models/Tickets';
 import mongoose from 'mongoose';
 jest.mock('../../nats-wrapper.ts');
 
@@ -8,8 +8,7 @@ it('returns an error if the user is not authenticated' , async() => {
     const id = new mongoose.Types.ObjectId().toHexString();
 
     const resp = await request(app)
-    .put(`/api/tickets/${id}`)
-    .send({title : 'Samp1', price : 200});
+    .delete(`/api/orders/${id}`)
     expect(resp.status).toEqual(403);
 })
 
@@ -18,65 +17,42 @@ it('returns a 404 error if the provided id is incorrect' , async() => {
 
     const id = new mongoose.Types.ObjectId().toHexString();
     const resp = await request(app)
-    .put(`/api/tickets/${id}`)
+    .delete(`/api/orders/${id}`)
     .set('Cookie', cookies)
-    .send({title : 'sample11', price : 105});
     expect(resp.status).toEqual(404);
 })
 
 it('returns a 403 error if the user doesnt own the ticket' , async() => {
-    let cookies = global.getSignUpCookie('201', 'a@a.com');
-    const ticketResp = await request(app)
-    .post('/api/tickets')
-    .set('Cookie', cookies)
-    .send({title : 'sample11', price : 105});
+    const ticket = Ticket.buildTicket({ title : 'Sample Title', price : 200 });
+    await ticket.save();
 
-    cookies = global.getSignUpCookie();
-    const resp = await request(app)
-    .put(`/api/tickets/${ticketResp.body.data.id}`)
+    let cookies = global.getSignUpCookie();
+    const orderResp = await request(app)
+    .post('/api/orders')
     .set('Cookie', cookies)
-    .send({title : 'sample11', price : 105});
+    .send({ ticketId : ticket.id })
+    .expect(201);
+    
+    cookies = global.getSignUpCookie('102');
+    const resp = await request(app)
+    .delete(`/api/orders/${orderResp.body.data.id}`)
+    .set('Cookie', cookies)
     expect(resp.status).toEqual(403);
 })
 
-
-it('returns bad request error when the request body is invalid' , async() => {
-    let cookies = global.getSignUpCookie();
-    const ticketResp = await request(app)
-    .post('/api/tickets')
-    .set('Cookie', cookies)
-    .send({title : 'sample11', price : 105});
-
-    const resp = await request(app)
-    .put(`/api/tickets/${ticketResp.body.data.id}`)
-    .set('Cookie', cookies)
-    .send({price : 105})
-    .expect(400);
-
-    await request(app)
-    .put(`/api/tickets/${ticketResp.body.data.id}`)
-    .set('Cookie', cookies)
-    .send({price : 105})
-    .expect(400);
-})
-
 it('returns successfully when the request body is correct' , async() => {
-    let cookies = global.getSignUpCookie();
-    const ticketResp = await request(app)
-    .post('/api/tickets')
-    .set('Cookie', cookies)
-    .send({title : 'sample11', price : 105});
-    expect(ticketResp.status).toEqual(201);
+    const ticket = Ticket.buildTicket({ title : 'Sample Title', price : 200 });
+    await ticket.save();
 
-    const putReq = await request(app)
-    .put(`/api/tickets/${ticketResp.body.data.id}`)
+    let cookies = global.getSignUpCookie('101');
+    const orderResp = await request(app)
+    .post('/api/orders')
     .set('Cookie', cookies)
-    .send({title : 'sampleAks', price : 200})
-    .expect(200);
+    .send({ ticketId : ticket.id })
+    .expect(201);
     
     const resp = await request(app)
-    .get(`/api/tickets/${ticketResp.body.data.id}`)
-
-    expect(resp.status).toEqual(200);
-    expect(resp.body.data.title).toEqual('sampleAks');
+    .delete(`/api/orders/${orderResp.body.data.id}`)
+    .set('Cookie', cookies)
+    expect(resp.status).toEqual(204);
 })
