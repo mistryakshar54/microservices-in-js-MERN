e787734bace01ad5d1c86e7b3ae114d9063faa92
Akshar <mistry.aks@gmail.com>
2021-04-02 20:22:39 +0530

    Remove ticket pubs

--- ticketing-app/orders/src/events/ticket-created-publisher.ts
+++
@@ -1,6 +0,0 @@
-import { Publisher, Subjects } from '@amdevcorp/ticketing-common';
-import { TicketCreatedEvent } from './ticket-created-listner';
-
-export class TicketCreatedPublisher extends Publisher<TicketCreatedEvent> {
-    readonly subject = Subjects.TicketCreated;
-} 
--- ticketing-app/orders/src/events/ticket-update-publisher.ts
+++
@@ -1,6 +0,0 @@
-import { Publisher, Subjects } from '@amdevcorp/ticketing-common';
-import { TicketCreatedEvent } from './ticket-created-listner';
-
-export class TicketUpdatedPublisher extends Publisher<TicketCreatedEvent> {
-    readonly subject = Subjects.TicketCreated;
-} 
